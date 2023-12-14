280c157bdf673fb73061737b477e1f639b43f3d2
Simão Bonvalot <73948790+izzypt@users.noreply.github.com>
2023-12-14 21:34:29 +0000

    Update Span.cpp
    Added Cnnonical form

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -19,6 +19,13 @@ template <typename T>
 class Span
 {
     public:
+        Span(void);
+        Span(unsigned int n);
+        Span(Span const &src);
+        ~Span(void);
+
+        Span &operator=(Span const &src);
+
         std::vector<unsigned int> storage;
         void add_number();
         void shortest_span(),
